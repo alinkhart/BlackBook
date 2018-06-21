65a7d99ceeb362afb7b27342bba9da8aa209b08f
Adam Linkhart <alinkhart@gmail.com>
2018-06-21 10:15:41 -0400

    Forms and MvvmCross working for iOS

---
+++ BlackBook.iOS/Setup/AppDelegate.cs
@@ -0,0 +1,16 @@
+﻿using BlackBook.Forms.Setup;
+using BlackBook.Setup;
+using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using UIKit;
+
+namespace BlackBook.iOS.Setup {
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp> {
+        
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options) {
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
--- BlackBook.iOS/Setup/Main.cs
+++ BlackBook.iOS/Setup/Main.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
-namespace BlackBook.iOS {
+namespace BlackBook.iOS.Setup {
     public class Application {
         // This is the main entry point of the application.
         static void Main(string[] args) {
